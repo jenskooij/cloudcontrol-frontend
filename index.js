4f0daf14f856427a009c08caef660c6741e79444
Jens Kooij <jens.kooij@incentro.com>
2017-08-31 20:18:06 +0200

    Updated version, removed @imports changed the path so it can work in a folder next to the cms repo

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ cc.targetCssFile='cms.css';
 cc.targetSassFile='cms.build.css';
 cc.targetJsFile='cms.build.js';
 cc.targetJsMinFile='cms.js';
+cc.targetJsPath='./../cloudcontrol/public/js/';
+cc.targetCssPath='./../cloudcontrol/public/css/';
 cc.run();
