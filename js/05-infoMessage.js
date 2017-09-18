58b8960983798b2140f248469e8133688e5c495f
Jens Kooij <jens.kooij@incentro.com>
2017-09-18 10:40:19 +0200

    Added new styling and behavior for infomessages

---
+++ js/05-infoMessage.js
@@ -0,0 +1,27 @@
+(function () {
+  "use strict";
+  var infoMessage = document.querySelector('.infoMessage'),
+    infoMessageTimeout,
+    afterFadeTimeout;
+
+  if (infoMessage !== null) {
+    infoMessage.addEventListener("mouseover", removeInfoMessageTimeout);
+    infoMessage.addEventListener("mouseout", addInfoMessageTimeout);
+    addInfoMessageTimeout();
+  }
+
+  function addInfoMessageTimeout() {
+    infoMessageTimeout = setTimeout(function () {
+      infoMessage.style.opacity = 0;
+      afterFadeTimeout = setTimeout(function () {
+        infoMessage.parentNode.removeChild(infoMessage);
+        clearTimeout(afterFadeTimeout);
+      }, 600);
+      clearTimeout(infoMessageTimeout);
+    }, 3500);
+  }
+
+  function removeInfoMessageTimeout () {
+    clearTimeout(infoMessageTimeout);
+  }
+})();
